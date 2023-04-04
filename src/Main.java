5afcae1c33e38a29913589cbf571c42e7ffe830b
OSS37 <suhanova375@gmail.com>
2023-04-05 00:10:33 +0300

    Вся ДЗ выполнена

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@ import java.util.Arrays;
 
 
 public class Main {
+    private static List<Integer> nums = new ArrayList<>(List.of(1, 2, 3, 4, 5, 4, 5, 6, 7));
+    private static List<String> words = new ArrayList<>(List.of("мандарин", "виноград", "слива", "мандарин", "яблоко", "банан", "виноград", "айва", "яблоко"));
+
     public static void main(String[] args) {
         task1();
         task2();
         task3();
-//        task4();
+        task4();
     }
 
+    //Метод, который выводит только нечетные числа в консоль
     public static void task1() {
         System.out.println("Задача 1");
-        List<Integer> nums = new ArrayList<>(List.of(1, 2, 3, 4, 5, 4, 5, 6, 7));
         for (int i = 0; i < nums.size(); i++) {
             if (nums.get(i) % 2 != 0) {
                 System.out.print(nums.get(i) + " ");
@@ -21,22 +24,31 @@ public class Main {
         System.out.println();
     }
 
+    //Метод, который выводит только четные числа без повторений в порядке возрастания.
     public static void task2() {
         System.out.println("Задача 2");
-        Set<Integer> numsSet = new HashSet<>(List.of(1, 4, 3, 5, 8, 3, 6, 2, 7, 2, 8)); // объявление сета
+        Set<Integer> numsSet = new HashSet<>(nums); // объявление сета
         ArrayList<Integer> num = new ArrayList<>(numsSet); // преобразование сета в лист (для избежания повторов)
         Collections.sort(num); // сортировка листа по возрастанию
         for (int i = 0; i < num.size(); i++) {
-            if (num.get(i) % 2 != 0) {
+            if (num.get(i) % 2 == 0) {
                 System.out.print(num.get(i) + " ");
             }
         }
+        System.out.println();
     }
 
+    //Метод, который выводит в консоль все уникальные слова из списка слов, в котором могут встречаться дубли
     public static void task3() {
         System.out.println("Задача 3");
-        List<String> nums = new ArrayList<>(List.of("мандарин", "виноград", "слива", "мандарин", "яблоко", "банан", "виноград", "айва"));
+        Set<String> setWords = new HashSet<>(words);
+        System.out.println(setWords);
+    }
 
-
+    //Метод, который выводит в консоль все количество дублей из списка слов
+    public static void task4() {
+        System.out.println("Задача 4");
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(words.size() - uniqueWords.size());
     }
 }
